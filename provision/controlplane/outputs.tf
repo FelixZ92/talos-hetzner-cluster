c8a9134b9285a89629350760b9af7a3116650874
Felix Zippel <contact@zippelf.com>
2023-03-12 00:43:30 +0100

    Test output

--- provision/controlplane/outputs.tf
+++ provision/controlplane/outputs.tf
@@ -1,3 +1,10 @@
 output "kubeapi_endpoint" {
   value = local.kubeapi_endpoint
 }
+
+output "controlplane_hosts" {
+  value = {
+    hostnames            = hcloud_server.control_plane.*.name
+    private_address      = hcloud_server_network.control_plane.*.ip
+  }
+}
--- provision/talos.tf
+++ provision/talos.tf
@@ -7,3 +7,16 @@ resource "talos_machine_configuration_controlplane" "machineconfig_cp" {
   cluster_endpoint = "https://${module.controlplane.kubeapi_endpoint}:6443"
   machine_secrets  = talos_machine_secrets.machine_secrets.machine_secrets
 }
+
+resource "talos_machine_configuration_worker" "machineconfig_worker" {
+  cluster_name     = var.cluster_name
+  cluster_endpoint = "https://${module.controlplane.kubeapi_endpoint}:6443"
+  machine_secrets  = talos_machine_secrets.machine_secrets.machine_secrets
+}
+
+resource "talos_client_configuration" "talosconfig" {
+  cluster_name    = var.cluster_name
+  machine_secrets = talos_machine_secrets.machine_secrets.machine_secrets
+  endpoints       = [for k, v in module.controlplane.controlplane_hosts.private_address : v]
+}
+

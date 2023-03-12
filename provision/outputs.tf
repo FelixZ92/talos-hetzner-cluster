f8d87c3f62ecaa551339121723aa847fdd3a3158
Felix Zippel <contact@zippelf.com>
2023-03-12 03:13:58 +0100

    Test output

--- provision/outputs.tf
+++ provision/outputs.tf
@@ -3,9 +3,5 @@ output "controlplane_hosts" {
 }
 
 output "zipmap_test" {
-  value = module.controlplane.kubeapi_endpoint
-}
-
-output "zipmap_test2" {
-  value = module.worker.zipmap_test
+  value = module.worker.*.zipmap_test
 }

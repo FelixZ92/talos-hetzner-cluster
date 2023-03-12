c3900723652891ba415f7fc5928328f9b52d5736
Felix Zippel <contact@zippelf.com>
2023-03-12 15:38:00 +0100

    Output talosconfig

--- provision/outputs.tf
+++ provision/outputs.tf
@@ -5,3 +5,23 @@ output "controlplane_hosts" {
 output "worker_hosts" {
   value = module.worker-pool-1.worker_hosts.worker
 }
+
+output "machineconfig_controlplane" {
+  value     = talos_machine_configuration_controlplane.machineconfig_cp.machine_config
+  sensitive = true
+}
+
+output "machineconfig_worker" {
+  value     = talos_machine_configuration_worker.machineconfig_worker.machine_config
+  sensitive = true
+}
+
+output "talosconfig" {
+  value     = talos_client_configuration.talosconfig.talos_config
+  sensitive = true
+}
+
+output "kubeconfig" {
+  value     = talos_cluster_kubeconfig.kubeconfig.kube_config
+  sensitive = true
+}

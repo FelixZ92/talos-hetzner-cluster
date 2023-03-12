ecf6ce7675439ddaeeccc8a6c2933f87126a8650
Felix Zippel <contact@zippelf.com>
2023-03-12 03:17:12 +0100

    Test output

--- provision/worker/outputs.tf
+++ provision/worker/outputs.tf
@@ -1,11 +1,6 @@
 output "worker_hosts" {
   value = {
-    "workers-${var.pool_name}" = [
-      for i, w in hcloud_server.worker : {
-        name            = w.name
-        private_address = hcloud_server_network.worker[i].ip
-      }
-    ]
+    workers = zipmap(hcloud_server.worker.*.name, hcloud_server_network.worker.*.ip)
   }
 }
 

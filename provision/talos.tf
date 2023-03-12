345a50a4a3282d0a0284f1c593d8f7586d2859e7
Felix Zippel <contact@zippelf.com>
2023-03-12 03:56:37 +0100

    Test output

--- provision/talos.tf
+++ provision/talos.tf
@@ -15,26 +15,26 @@ resource "talos_machine_configuration_worker" "machineconfig_worker" {
 }
 
 #resource "talos_client_configuration" "talosconfig" {
-  #cluster_name    = var.cluster_name
-  #machine_secrets = talos_machine_secrets.machine_secrets.machine_secrets
-  #endpoints       = module.controlplane.controlplane_hosts.controlplanes.*.private_address
+#  cluster_name    = var.cluster_name
+#  machine_secrets = talos_machine_secrets.machine_secrets.machine_secrets
+#  endpoints       = module.controlplane.test.controlplanes.*.private_address
 #}
 
-#resource "talos_machine_configuration_apply" "cp_config_apply" {
-#  talos_config          = talos_client_configuration.talosconfig.talos_config
-#  machine_configuration = talos_machine_configuration_controlplane.machineconfig_cp.machine_config
-#  for_each              = module.controlplane.controlplane_hosts.controlplanes
-#  endpoint              = each.value.private_address
-#  node                  = each.value.name
-#  config_patches = [
-#    file("${path.module}/patches/common/rotate-certs.yaml"),
-#    templatefile("${path.module}/patches/controlplane/extra-sans.yaml", {
-#      loadbalancer_ip = module.controlplane.kubeapi_endpoint
-#    }),
-#    file("${path.module}/patches/controlplane/cloud-provider-hetzner.yaml"),
-#    file("${path.module}/patches/controlplane/metrics-server.yaml"),
-#  ]
-#}
+resource "talos_machine_configuration_apply" "cp_config_apply" {
+  talos_config          = talos_client_configuration.talosconfig.talos_config
+  machine_configuration = talos_machine_configuration_controlplane.machineconfig_cp.machine_config
+  for_each              = module.controlplane.test.controlplanes
+  endpoint              = each.value.private_address
+  node                  = each.value.name
+  config_patches = [
+    file("${path.module}/patches/common/rotate-certs.yaml"),
+    templatefile("${path.module}/patches/controlplane/extra-sans.yaml", {
+      loadbalancer_ip = module.controlplane.kubeapi_endpoint
+    }),
+    file("${path.module}/patches/controlplane/cloud-provider-hetzner.yaml"),
+    file("${path.module}/patches/controlplane/metrics-server.yaml"),
+  ]
+}
 
 #
 #resource "talos_machine_configuration_apply" "worker_config_apply" {

83eb75b30936ebd2dca9675fc5546de9c1d08543
Felix Zippel <contact@zippelf.com>
2023-03-18 01:21:42 +0100

    Specify datacenter

--- provision/controlplane/instance/main.tf
+++ provision/controlplane/instance/main.tf
@@ -8,7 +8,7 @@ resource "hcloud_primary_ip" "primary_ip" {
   name          = "${var.cluster_name}-control-plane-${var.index}"
   type          = "ipv4"
   assignee_type = "server"
-  datacenter = var.datacenter
+  datacenter    = var.datacenter
   auto_delete   = false
   labels        = {
     "cluster" = var.cluster_name
@@ -20,16 +20,16 @@ resource "talos_machine_configuration_controlplane" "machineconfig_cp" {
   cluster_name     = var.cluster_name
   cluster_endpoint = "https://${var.loadbalancer_public_ip}:6443"
   machine_secrets  = var.machine_secrets
-  config_patches = [
+  config_patches   = [
     file("${path.module}/patches/common/interfaces.yaml"),
     templatefile("${path.module}/patches/common/kubelet-valid-subnets.yaml", {
       ip_range = var.vpc_cidr
     }),
     templatefile("${path.module}/patches/common/machine-cert-sans.yaml", {
-      public_loadbalancer_ip = var.loadbalancer_public_ip
+      public_loadbalancer_ip  = var.loadbalancer_public_ip
       private_loadbalancer_ip = var.loadbalancer_private_ip
-      public_ip = hcloud_primary_ip.primary_ip.ip_address
-      private_ip = var.private_ip
+      public_ip               = hcloud_primary_ip.primary_ip.ip_address
+      private_ip              = var.private_ip
     }),
     templatefile("${path.module}/patches/common/node-ip.yaml", {
       private_ip = var.private_ip
@@ -40,10 +40,10 @@ resource "talos_machine_configuration_controlplane" "machineconfig_cp" {
       ip_range = var.vpc_cidr
     }),
     templatefile("${path.module}/patches/controlplane/extra-sans.yaml", {
-      public_loadbalancer_ip = var.loadbalancer_public_ip
+      public_loadbalancer_ip  = var.loadbalancer_public_ip
       private_loadbalancer_ip = var.loadbalancer_private_ip
-      public_ip = hcloud_primary_ip.primary_ip.ip_address
-      private_ip = var.private_ip
+      public_ip               = hcloud_primary_ip.primary_ip.ip_address
+      private_ip              = var.private_ip
     }),
     file("${path.module}/patches/controlplane/metrics-server.yaml"),
   ]
@@ -53,7 +53,7 @@ resource "hcloud_server" "control_plane" {
   name               = "${var.cluster_name}-control-plane-${var.index}"
   server_type        = var.control_plane_type
   image              = var.image
-  location           = var.datacenter
+  datacenter         = var.datacenter
   placement_group_id = var.placement_group
   user_data          = talos_machine_configuration_controlplane.machineconfig_cp.machine_config
   labels             = {
--- provision/worker/instance/main.tf
+++ provision/worker/instance/main.tf
@@ -8,7 +8,7 @@ resource "hcloud_primary_ip" "primary_ip" {
   name          = "${var.cluster_name}-${var.pool_name}-worker-${var.index}"
   type          = "ipv4"
   assignee_type = "server"
-  datacenter = var.datacenter
+  datacenter    = var.datacenter
   auto_delete   = false
   labels        = {
     "cluster" = var.cluster_name
@@ -20,16 +20,16 @@ resource "talos_machine_configuration_worker" "machineconfig_worker" {
   cluster_name     = var.cluster_name
   cluster_endpoint = "https://${var.loadbalancer_public_ip}:6443"
   machine_secrets  = var.machine_secrets
-  config_patches = [
+  config_patches   = [
     file("${path.module}/patches/common/interfaces.yaml"),
     templatefile("${path.module}/patches/common/kubelet-valid-subnets.yaml", {
       ip_range = var.vpc_cidr
     }),
     templatefile("${path.module}/patches/common/machine-cert-sans.yaml", {
-      public_loadbalancer_ip = var.loadbalancer_public_ip
+      public_loadbalancer_ip  = var.loadbalancer_public_ip
       private_loadbalancer_ip = var.loadbalancer_private_ip
-      public_ip = hcloud_primary_ip.primary_ip.ip_address
-      private_ip = var.private_ip
+      public_ip               = hcloud_primary_ip.primary_ip.ip_address
+      private_ip              = var.private_ip
     }),
     templatefile("${path.module}/patches/common/node-ip.yaml", {
       private_ip = var.private_ip
@@ -42,7 +42,7 @@ resource "hcloud_server" "worker" {
   name               = "${var.cluster_name}-${var.pool_name}-worker-${var.index}"
   server_type        = var.worker_type
   image              = var.image
-  location           = var.datacenter
+  datacenter         = var.datacenter
   placement_group_id = var.placement_group
   user_data          = talos_machine_configuration_worker.machineconfig_worker.machine_config
   labels             = {

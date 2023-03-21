349cc01d7d8f0699c344c0a45b940824fb66d8d5
Felix Zippel <contact@zippelf.com>
2023-03-22 00:19:01 +0100

    Fix k8s provider

--- provision/kubernetes.tf
+++ provision/kubernetes.tf
@@ -1,11 +1,11 @@
-#provider kubernetes {
-#  host = yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["server"]
-#
-#  cluster_ca_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["certificate-authority-data"])
-#
-#  client_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-certificate-data"])
-#  client_key         = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-key-data"])
-#}
+provider kubernetes {
+  host = yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["server"]
+
+  cluster_ca_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["certificate-authority-data"])
+
+  client_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-certificate-data"])
+  client_key         = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-key-data"])
+}
 #
 #provider "helm" {
 #  kubernetes {

bd49d080586d49c96addb51deacbfa0fe19c96f7
Felix Zippel <contact@zippelf.com>
2023-03-22 00:12:27 +0100

    Test again with yaml decoding

--- provision/kubernetes.tf
+++ provision/kubernetes.tf
@@ -1,42 +1,42 @@
-provider kubernetes {
-  host = yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["server"]
-
-  cluster_ca_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["certificate-authority-data"])
-
-  client_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-certificate-data"])
-  client_key         = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-key-data"])
-}
-
-provider "helm" {
-  kubernetes {
-    host = yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["server"]
-
-    cluster_ca_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["certificate-authority-data"])
-
-    client_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-certificate-data"])
-    client_key         = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-key-data"])
-  }
-}
-
-resource "time_sleep" "wait_30_for_api_server" {
-  depends_on = [talos_machine_bootstrap.bootstrap]
-
-  create_duration = "60s"
-}
-
-resource "helm_release" "cilium" {
-  depends_on = [time_sleep.wait_30_for_api_server]
-  chart      = "cilium"
-  namespace  = "kube-system"
-  repository = "https://helm.cilium.io/"
-  version    = "1.13.1"
-  name       = "cilium"
-  values     = [
-    templatefile("cilium.yaml", {
-      private_loadbalancer_ip = hcloud_load_balancer_network.load_balancer.ip
-    })
-  ]
-}
+#provider kubernetes {
+#  host = yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["server"]
+#
+#  cluster_ca_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["certificate-authority-data"])
+#
+#  client_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-certificate-data"])
+#  client_key         = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-key-data"])
+#}
+#
+#provider "helm" {
+#  kubernetes {
+#    host = yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["server"]
+#
+#    cluster_ca_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["clusters"][0]["cluster"]["certificate-authority-data"])
+#
+#    client_certificate = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-certificate-data"])
+#    client_key         = base64decode(yamldecode(talos_cluster_kubeconfig.kubeconfig.kube_config)["users"][0]["user"]["client-key-data"])
+#  }
+#}
+#
+#resource "time_sleep" "wait_30_for_api_server" {
+#  depends_on = [talos_machine_bootstrap.bootstrap]
+#
+#  create_duration = "60s"
+#}
+#
+#resource "helm_release" "cilium" {
+#  depends_on = [time_sleep.wait_30_for_api_server]
+#  chart      = "cilium"
+#  namespace  = "kube-system"
+#  repository = "https://helm.cilium.io/"
+#  version    = "1.13.1"
+#  name       = "cilium"
+#  values     = [
+#    templatefile("cilium.yaml", {
+#      private_loadbalancer_ip = hcloud_load_balancer_network.load_balancer.ip
+#    })
+#  ]
+#}
 
 #
 #resource "kubernetes_secret" "hcloud_token" {
